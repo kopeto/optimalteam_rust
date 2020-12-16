230f5a0730bace14df18921294423dbd030df6bd
kopeto <ander.elosegi@gmail.com>
2020-12-16 21:39:42 +0100

    some

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,8 @@
 use std::io::prelude::*;
 
-#[derive(PartialEq, Eq)]
-enum Discipline
-{
-    C1M,
-    C1W,
-    K1M,
-    K1W,
-}
+mod team;
 
-struct Athlete
-{
-    name: String,
-    price: u32,
-    points: u32,
-    country: String,
-    discipline: Discipline,
-}
-
-struct Team<'a> 
-{
-    max_size : u32,
-
-    team : Vec<&'a Athlete>,
-}
-
-impl<'a> Team<'a>
-{
-    fn points(&self) -> u32 
-    {
-        let mut points : u32 = 0;
-        for ath in &self.team 
-        {
-            points += ath.points;
-        }
-        points
-    }
-
-    fn price(&self) -> u32 
-    {
-        let mut price : u32 = 0;
-        for ath in &self.team 
-        {
-            price += ath.price;
-        }
-        price
-    }
-
-    fn print(&self)
-    {
-        for ath in &self.team
-        {
-            println!("{}|{} {} {} {}",match ath.discipline {
-                Discipline::C1M => "C1M",
-                Discipline::K1M => "K1M",
-                Discipline::C1W => "C1W",
-                Discipline::K1W => "K1W",
-            }, ath.name, ath.points, ath.country, ath.price);
-        }
-        println!("Total price: {}", &self.price());
-        println!("Total points: {}", &self.points());
-    }
-
-    fn at_least_1_in(&self, disc : Discipline) -> bool
-    {
-        for ath in &self.team
-        {
-            if disc == ath.discipline
-            {
-                return true;
-            }
-        }
-        false
-    }
-
-    fn at_least_1_per_discipline(&self) -> bool
-    {
-        self.at_least_1_in(Discipline::C1M) 
-        && self.at_least_1_in(Discipline::C1W) 
-        && self.at_least_1_in(Discipline::K1M) 
-        && self.at_least_1_in(Discipline::K1W) 
-    }
-
-    fn per_country_max(&self,max_per_country : u32) -> bool
-    {
-        let mut map = std::collections::HashMap::<String,u32>::new();
-        for ath in &self.team
-        {
-            let country = map.get(&ath.country);
-            match country {
-                None => {map.insert(String::from(&ath.country), 1); ()},
-                Some(n) =>
-                {
-                    if n < &max_per_country
-                    {
-                        let a = n+1;
-                        map.insert(String::from(&ath.country), a);
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-        true
-    }
-}
-
-fn get_paddlers() -> Vec<Athlete>
+fn get_paddlers() -> Vec<team::Athlete>
 {
     let mut line = String::new(); 
     std::io::stdin().read_line(&mut line).unwrap();
@@ -116,7 +10,7 @@ fn get_paddlers() -> Vec<Athlete>
     let _n : u32 = line.trim().parse().unwrap();
 
 
-    let mut athletes : Vec<Athlete> = Vec::new();
+    let mut athletes : Vec<team::Athlete> = Vec::new();
 
     for line in std::io::stdin().lock().lines() {
         let line = line.unwrap();
@@ -127,18 +21,18 @@ fn get_paddlers() -> Vec<Athlete>
         let country = iter.next().unwrap();
         let discipline = iter.next().unwrap();
 
-        let ath = Athlete{
+        let ath = team::Athlete{
             name:  String::from(name),
             price: price.parse::<u32>().unwrap(),
             points: points.parse::<u32>().unwrap(),
             country: String::from(country),
             discipline: match discipline
             {
-                "C1M" => Discipline::C1M,
-                "C1W" => Discipline::C1W,
-                "K1M" => Discipline::K1M,
-                "K1W" => Discipline::K1W,
-                _ => Discipline::C1M,
+                "C1M" => team::Discipline::C1M,
+                "C1W" => team::Discipline::C1W,
+                "K1M" => team::Discipline::K1M,
+                "K1W" => team::Discipline::K1W,
+                _ => team::Discipline::C1M,
             }
         };
 
@@ -150,9 +44,9 @@ fn get_paddlers() -> Vec<Athlete>
 }
 
 fn solver<'a> ( 
-            mut candidate_team : &mut Team<'a>,
-            mut current_team : &mut Team<'a>,
-            athletes : &'a[Athlete],
+            mut candidate_team : &mut team::Team<'a>,
+            mut current_team : &mut team::Team<'a>,
+            athletes : &'a [team::Athlete],
             remaining_budget: u32,
             max_per_country: u32,
         ) 
@@ -198,19 +92,19 @@ fn solver<'a> (
         
 fn main() {
     
-    let team_max_size = 8;
+    let team_max_size = 7;
     let initial_budget = 25000;
     let max_per_country = 2;
     
-    let athletes : Vec<Athlete> = get_paddlers();
+    let athletes : Vec<team::Athlete> = get_paddlers();
 
-    let mut best_team : Team = Team{
+    let mut best_team = team::Team{
         max_size: team_max_size,
-        team : Vec::<&Athlete>::new(),
+        team : Vec::<&team::Athlete>::new(),
     };
-    let mut current_team : Team = Team{
+    let mut current_team = team::Team{
         max_size: team_max_size,
-        team : Vec::<&Athlete>::new(),
+        team : Vec::<&team::Athlete>::new(),
     };
 
     let timer = std::time::Instant::now();
---
+++ src/team.rs
@@ -0,0 +1,108 @@
+
+#[derive(PartialEq, Eq)]
+pub enum Discipline
+{
+    C1M,
+    C1W,
+    K1M,
+    K1W,
+}
+
+pub struct Athlete
+{
+    pub name: String,
+    pub price: u32,
+    pub points: u32,
+    pub country: String,
+    pub discipline: Discipline,
+}
+
+pub struct Team<'a> 
+{
+    pub max_size : u32,
+
+    pub team : Vec<&'a Athlete>,
+}
+
+impl<'a> Team<'a>
+{
+    pub fn points(&self) -> u32 
+    {
+        let mut points : u32 = 0;
+        for ath in &self.team 
+        {
+            points += ath.points;
+        }
+        points
+    }
+
+    pub fn price(&self) -> u32 
+    {
+        let mut price : u32 = 0;
+        for ath in &self.team 
+        {
+            price += ath.price;
+        }
+        price
+    }
+
+    pub fn print(&self)
+    {
+        for ath in &self.team
+        {
+            println!("{}|{} {} {} {}",match ath.discipline {
+                Discipline::C1M => "C1M",
+                Discipline::K1M => "K1M",
+                Discipline::C1W => "C1W",
+                Discipline::K1W => "K1W",
+            }, ath.name, ath.points, ath.country, ath.price);
+        }
+        println!("Total price: {}", &self.price());
+        println!("Total points: {}", &self.points());
+    }
+
+    pub fn at_least_1_in(&self, disc : Discipline) -> bool
+    {
+        for ath in &self.team
+        {
+            if disc == ath.discipline
+            {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn at_least_1_per_discipline(&self) -> bool
+    {
+        self.at_least_1_in(Discipline::C1M) 
+        && self.at_least_1_in(Discipline::C1W) 
+        && self.at_least_1_in(Discipline::K1M) 
+        && self.at_least_1_in(Discipline::K1W) 
+    }
+
+    pub fn per_country_max(&self,max_per_country : u32) -> bool
+    {
+        let mut map = std::collections::HashMap::<String,u32>::new();
+        for ath in &self.team
+        {
+            let country = map.get(&ath.country);
+            match country {
+                None => {map.insert(String::from(&ath.country), 1); ()},
+                Some(n) =>
+                {
+                    if n < &max_per_country
+                    {
+                        let a = n+1;
+                        map.insert(String::from(&ath.country), a);
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        true
+    }
+}
